393296774eb7de241ee54b92682a17fd20e8466c
Martin Suchan <2572779s@student.gla.ac.uk>
2023-10-19 22:15:35 +0100

    renamed graph_generation.py to maze.py. Changed graph generation to a class called Maze. Added some basic methods to the maze class. Added a main.py class, including methods. Original functionality of graph_generation.py preserved, except for generating graph. Will be pushing that soon as well.

---
+++ main.py
@@ -0,0 +1,65 @@
+"""
+This module functions as the main call. Generates GUI to wrap around graph generation. Shows the generated graph in tkinter.
+"""
+import tkinter as tk
+from maze import Maze
+
+class Main():
+    """
+    A main class responsible for creating a GUI window that wraps around our maze, so that it can be shown in tkinter.
+    """
+    def __init__ (self, window_size_x = "500", window_size_y = "500"):
+        """
+        Constructor for main.
+        """
+        # Set up some variables
+        self.window_size_x = window_size_x
+        self.window_size_y = window_size_y
+        self.maze = Maze()
+        # Set up root
+        self.root = tk.Tk()
+        self.root.geometry (f"{self.window_size_x}x{self.window_size_y}")
+        self.root.title("Mousemaze")
+
+        # Creating frames
+        self.frame_input = tk.Frame(self.root, width = window_size_x)
+        self.frame_input.pack()
+        self.frame_maze = tk.Frame(self.root, width = window_size_x)
+        self.frame_maze.pack()
+        # Input frame element creation
+        self.label_entry = tk.Label(self.frame_input, text="Input a list of tuples in the format x1 y2,x2 y2,... to set nodes to be removed from maze.")
+        self.label_entry.pack()
+        self.entry_tuple_list = tk.Entry(self.frame_input)
+        self.entry_tuple_list.pack(side = tk.LEFT)
+        self.button_submit = tk.Button(self.frame_input, text = "submit tuples", command=self.submit_tuples)
+        self.button_submit.pack(side = tk.LEFT)
+        self.label_warning = tk.Label(self.frame_input, text = "test")
+        # Run the root
+        self.root.mainloop()
+
+    def submit_tuples(self) -> None:
+        """Takes single string input from entry, does basic check, sets obstacles for maze if success, else just warns user. Returns whether operation was succesful or not."""
+        tuples_string = self.entry_tuple_list.get()
+        success, tuple_list = self.cs_tuples(tuples_string)
+        if(success):
+            self.label_warning["text"] = "Submitting tuples was succesful!"
+            self.maze.set_obstacles(tuple_list)
+        else:
+            self.label_warning["text"] = "Failure to submit tuples. Check format and try again."
+        self.label_warning.pack(side = tk.BOTTOM)
+    
+    def cs_tuples(self,cs_tuple_string) -> (bool,[()]):
+        if(cs_tuple_string == ""):
+            return (False, [()])
+        tuple_list =[]
+        tuples_string_list = cs_tuple_string.split(',')
+        print(f"TUPLES STRING LIST :: {tuples_string_list}")
+        for t in tuples_string_list:
+            x, y = t.split(' ')
+            new_t = (int(x), int(y))
+            tuple_list.append(new_t)
+        return (True,tuple_list)
+
+
+if __name__ == "__main__":
+    main = Main()
---
+++ maze.py
@@ -0,0 +1,43 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class Maze():
+    """Class that initiates a 13 by 13 graph/grid."""
+    def __init__(self, obstacles = [()]):
+        # graph creation - taking width and height as parameters
+        self.grid = nx.grid_2d_graph(13,13)
+        #print(list(G.nodes))
+        # List of nodes to be removed from graph
+        self.obstacles = obstacles
+
+    def print_nodes(self):
+        print(list(self.grid.nodes))
+
+    def set_obstacles(self, tuples):
+        # the list of nodes that should be deleted 
+        # passed as list of tuples where each tuple represents a node by x, y coordinates. Eg. [(0,0),(1,1)]
+        self.obstacles = tuples
+
+    def print_obstacles(self):
+        print(self.obstacles)
+
+    def remove_nodes(self):
+        for obstacle in self.obstacles:
+            self.grid.remove_node(obstacle)
+
+    def visualise(self):
+        # graph visualisation
+        positions = {}
+        for i in range(13):
+            for j in range(13):
+                if (i,j) in self.grid.nodes:
+                    positions[(i,j)] = (5*i,5*(13-j))
+        nx.draw(self.grid, positions)
+
+
+if __name__ == "__main__":
+    maze = Maze()
+    maze.set_obstacles()
+    maze.remove_nodes()
+    maze.visualise()
+    plt.show()
